3d47830a0e3739f082aa74a26a269a565b2abad1
afoote99 <afoote99@gmail.com>
2024-01-31 08:43:44 -0700

    Getting file to Jake

---
+++ TheGoods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tick_Tack_Toe2_12
+{
+    internal class TheGoods
+    {
+        //Print the board based on info passed to it
+        public string PutInPlace(//parameter will be the )
+        {
+            //take list object
+            List<object> BoardArray = new List<object>
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9
+            };
+
+
+            //
+        }
+
+    }
+}
